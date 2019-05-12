22c7af5210625288b43a51f5382b7d43a5f60227
Rich Harris <richard.a.harris@gmail.com>
2019-05-12 12:17:39 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,122 @@
+<script>
+	import { onMount } from 'svelte';
+	import {
+		Scene,
+		Material,
+		AmbientLight,
+		PointLight,
+		DirectionalLight,
+		OrbitControls,
+		PerspectiveCamera,
+		Group,
+		Plane,
+		Cone,
+		Cube,
+		Sphere,
+		Overlay
+	} from '@sveltejs/gl';
+	import SvelteBox from './SvelteBox.svelte';
+	import { logotype } from './images.js';
+
+	const prime = 0xff3e00;
+	const second = 0x676778;
+	const flash = 0x40b3ff;
+
+	let light = {};
+
+	onMount(() => {
+		let frame;
+
+		const loop = () => {
+			frame = requestAnimationFrame(loop);
+
+			light.x = 3 * Math.sin(Date.now() * 0.001);
+			light.y = 5 + 3 * Math.sin(Date.now() * 0.0004);
+			light.z = 3 * Math.cos(Date.now() * 0.002);
+		};
+
+		loop();
+
+		return () => cancelAnimationFrame(frame);
+	});
+</script>
+
+<style>
+	.info {
+		position: absolute;
+		top: 1em;
+		left: 1em;
+		background-color: rgba(255,255,255,0.7);
+		padding: 1em;
+		border-radius: 2px;
+	}
+</style>
+
+<Scene>
+	<OrbitControls maxPolarAngle={Math.PI / 2} let:location let:target>
+		<PerspectiveCamera {location} {target} near={0.01}/>
+	</OrbitControls>
+
+	<AmbientLight intensity={0.6}/>
+
+	<Sphere
+		location={[light.x,light.y + 0.2,light.z]}
+		color={second}
+		scale={0.1}
+	/>
+	<PointLight
+		location={[light.x,light.y,light.z]}
+		color={second}
+		intensity={0.2}
+	/>
+
+	<DirectionalLight direction={[-1,-1,-1]} intensity={0.8}/>
+
+	<Plane
+		color={0xffffff}
+		location={[0,-0.01,0]}
+		rotation={[-90,0,0]}
+		scale={10}
+	/>
+
+	<!-- TODO figure out alpha channels in textures -->
+	<!-- <Plane
+		color={0xffffff}
+		map={logotype}
+		location={[0,1,-2]}
+		scale={[4, 1, 0]}
+	/> -->
+
+	<Sphere
+		color={prime}
+		alpha={0.7}
+		subdivisions={4}
+		scale={0.3}
+		location={[0,0.3,0]}
+	/>
+
+	<Sphere
+		color={flash}
+		alpha={0.7}
+		subdivisions={4}
+		scale={0.7}
+		location={[-1,0.7,-1]}
+	/>
+
+	<Sphere
+		color={second}
+		alpha={0.7}
+		subdivisions={4}
+		scale={0.2}
+		location={[-0.8,0.2,0]}
+	/>
+
+	<SvelteBox location={[-2,0.5,0]} rotation={[0,-20,0]}/>
+
+	<SvelteBox location={[1,0.75,-1]} rotation={[0,30,0]} scale={1.5}/>
+</Scene>
+
+<div class="info">
+	<h1><a target="_blank" rel="noopener" href="https://github.com/sveltejs/gl">@sveltejs/gl</a></h1>
+	<p>The code for this demo lives <a target="_blank" rel="noopener" href="https://github.com/rich-harris/svelte-gl-demo">here</a></p>
+</div>
---
+++ src/SvelteBox.svelte
@@ -0,0 +1,80 @@
+<script>
+	import * as yootils from 'yootils';
+	import { onMount } from 'svelte';
+	import { Box, Material } from '@sveltejs/gl';
+	import { logo } from './images.js';
+
+	const material = new Material({
+		color: [1, 1, 1]
+	});
+
+	const img = new Image();
+	img.src = logo;
+
+	img.onload = () => {
+		const aspect = img.naturalWidth / img.naturalHeight;
+		const s = 512;
+
+		const bitmap = yootils.createSprite(s, s, ctx => {
+			ctx.fillStyle = 'white';
+			ctx.fillRect(0, 0, s, s);
+
+			ctx.strokeStyle = '#eee';
+			ctx.lineWidth = 4;
+			ctx.strokeRect(0, 0, s, s);
+
+			const width = s * 0.7;
+			const height = width / aspect;
+
+			ctx.drawImage(
+				img,
+				(s - width) / 2,
+				(s - height) / 2,
+				width,
+				height
+			);
+		});
+
+		const canvas = document.createElement('canvas');
+		canvas.width = canvas.height = s * 4;
+
+		// use this layout:
+		//   T
+		// L F R B
+		//   B
+
+		const ctx = canvas.getContext('2d');
+
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+		ctx.drawImage(bitmap, 1 * s, 0 * s); // top
+		ctx.drawImage(bitmap, 0 * s, 1 * s); // left
+		ctx.drawImage(bitmap, 1 * s, 1 * s); // front
+		ctx.drawImage(bitmap, 2 * s, 1 * s); // right
+		ctx.drawImage(bitmap, 3 * s, 1 * s); // back
+		ctx.drawImage(bitmap, 1 * s, 2 * s); // bottom
+
+		// ctx.fillStyle = 'white';
+		// ctx.font = `bold ${s / 5}px sans-serif`;
+		// ctx.textBaseline = 'middle';
+		// ctx.textAlign = 'center';
+
+		// ctx.fillText('top',    3/2 * s, 1/2 * s);
+		// ctx.fillText('left',   1/2 * s, 3/2 * s);
+		// ctx.fillText('front',  3/2 * s, 3/2 * s);
+		// ctx.fillText('right',  5/2 * s, 3/2 * s);
+		// ctx.fillText('back',   7/2 * s, 3/2 * s);
+		// ctx.fillText('bottom', 3/2 * s, 5/2 * s);
+
+		material.map = canvas;
+
+		// document.body.appendChild(canvas);
+	};
+
+	img.onerror = (event) => {
+		console.error(event);
+	};
+</script>
+
+<Box {...$$props} {material}/>
